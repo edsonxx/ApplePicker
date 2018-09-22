035d37a7471f40526e076231b193081d9776ce80
Edson Lin <43080740+edsonxx@users.noreply.github.com>
2018-09-21 20:05:51 -0400

    Finished Project
    Completed Highscore and score with highscore carrying over the next game.

---
+++ Assets/HighScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    public static int score = 1000;
+
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+    }
+
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        { // d
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+}
--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Apple : MonoBehaviour
-{
+public class Apple : MonoBehaviour { 
+    [Header("Set in Inspector")]
     public static float bottomY = -20f; // a
     void Update()
     {
         if (transform.position.y < bottomY)
         {
             Destroy(this.gameObject); // b
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker> (); // b
+            apScript.AppleDestroyed();
         }
     }
 }
-	
